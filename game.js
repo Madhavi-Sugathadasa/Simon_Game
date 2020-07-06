bb208b98aa433618378b547194c2829353c6fd48
Madhavi-Sugathadasa <madhavi.sug@gmail.com>
2020-07-06 19:54:08 +1000

    updated js, added game over styles.

--- game.js
+++ game.js
@@ -70,7 +70,12 @@ function checkAnswer(currentLevel) {
 
     } else {
 
-        console.log("wrong");
+        playSound('wrong');
+        $('body').addClass('game-over');
+        window.setTimeout(function () {
+            $('body').removeClass('game-over');
+        }, 200);
+        $("#level-title").text("Game Over, Press Any Key to Restart" );
 
     }
 
